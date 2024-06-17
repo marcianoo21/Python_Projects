daa25defb28333cb3618fa03c52cfc726dc1c677
killu21 <mgrzelak218@gmail.com>
2024-06-18 00:34:01 +0200

    still has to be corrected

--- blog_template_updated/main.py
+++ blog_template_updated/main.py
@@ -1,26 +1,32 @@
 from flask import Flask, render_template
 import requests
 
-posts = requests.get("https://api.npoint.io/674f5423f73deab1e9a7").json()
+posts = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json").json()
 #TODO wyłuskac id, title, subtitle, body
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
-
-@app.route("/index.html")
+@app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
